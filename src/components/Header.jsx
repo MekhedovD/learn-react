9e418e5dceca0ca801caf75b102d9750dd32f3c4
Dmitry <mekhedov.d@mail.ru>
2020-10-13 12:50:20 +0300

    add in git

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRN5IdkZGOBECwRAqpYx6HH_Pr4Wy164El1Cg&usqp=CAU" />
+      </header>
+  )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+const Profile = () => {
+  return (
+    <div className="content">
+        <div>
+          <img src="https://tinyjpg.com/images/social/website.jpg" />
+        </div>
+        <div>ava + description</div>
+        <div>
+          My post
+          <div>New post</div>
+          <div>
+            <div>Post 1</div>
+            <div>Post 2</div>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default Profile;
